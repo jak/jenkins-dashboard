3441e00f5cbae12fbbe6d3c10f12ff13cee1d4ae
Jak Spalding <jak.spalding@bbc.co.uk>
2013-09-10 13:32:40 +0100

    Correct settings

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ if(typeof window.jenkinsDash === 'undefined') window.jenkinsDash = {};
 jenkinsDash.settings = [
   {
     name: 'ci',
-    host: 'https://ci-app.dev.bbc.co.uk',
-    view: 'All'
+    host: 'https://ci-app.int.bbc.co.uk/hudson',
+    view: 'iplayer'
   }
 ];
